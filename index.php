a0ffd9555ac0553a95b3879678934b0e07b7fb4d
Norbert Winkler <norbert.winkler@dataformers.at>
2018-03-01 22:44:41 +0100

    initial commit - first #out

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include '../header.php';
+?>
+
+<h1>#OUT</h1>
+<h2 style="font-style: italic;">[paʊnd aʊt]</h2>
+
+<div class="content">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-2">
+            <div class="row">
+                <a href="01_introduction/index.html" style="font-size: 14pt;">01 - Introduction (start here)</a>
+            </div>
+            <div class="row">
+                <a href="01_introduction/index.html"><img src="01_introduction/thumb.PNG"></a>
+            </div>            
+        </div>
+    </div>
+    
+</div>
+
+<?php
+include '../footer.php';
+?>
