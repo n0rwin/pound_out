a387eadd0575aaf240da1f894a64277cf3a09a1a
Norbert Winkler <norbert.winkler@dataformers.at>
2018-03-01 22:52:05 +0100

    added link to github repo

--- index.php
+++ index.php
@@ -7,16 +7,18 @@ include '../header.php';
 
 <div class="content">
     <div class="row">
+    <p>All the code for my #OUTs can be found at <a href="https://github.com/n0rwin/pound_out" style="font-weight: bold;">my github repository</a></p>
+    </div>
+    <div class="row">
         <div class="col-md-4 col-md-offset-2">
             <div class="row">
                 <a href="01_introduction/index.html" style="font-size: 14pt;">01 - Introduction (start here)</a>
             </div>
             <div class="row">
                 <a href="01_introduction/index.html"><img src="01_introduction/thumb.PNG"></a>
-            </div>            
+            </div>
         </div>
     </div>
-    
 </div>
 
 <?php
