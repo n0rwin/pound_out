20bf143ed6f5e250ca7ed321c50d32ecb6a5f2b5
n0rwin <me@norwin.at>
2018-03-11 18:31:37 +0100

    added #OUT 2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include '../header.php';
 
 <div class="content">
     <div class="row">
-    <p>All the code for my #OUTs can be found at <a href="https://github.com/n0rwin/pound_out" style="font-weight: bold;">my github repository</a></p>
+        <p>All the code for my #OUTs can be found at <a href="https://github.com/n0rwin/pound_out" style="font-weight: bold;">my github repository</a></p>
     </div>
     <div class="row">
         <div class="col-md-4 col-md-offset-2">
@@ -17,6 +17,16 @@ include '../header.php';
             <div class="row">
                 <a href="01_introduction/index.html"><img src="01_introduction/thumb.PNG"></a>
             </div>
+            <p class="row">Published on 2018-02-28</p>
+        </div>
+        <div class="col-md-4">
+            <div class="row">
+                <a href="02_good_artists/index.html" style="font-size: 14pt;">02 - Good Artists ...</a>
+            </div>
+            <div class="row">
+                <a href="02_good_artists/index.html"><img src="02_good_artists/thumb.PNG"></a>
+            </div>
+            <p class="row">Published on 2018-03-11</p>
         </div>
     </div>
 </div>
