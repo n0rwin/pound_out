b5c6d1a8f06aa373e467f5851b9ae1c87eb40ddb
Norbert Winkler <norbert.winkler@dataformers.at>
2018-03-18 17:21:43 +0100

    pound out 3

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ include '../header.php';
 <h1>#OUT</h1>
 <h2 style="font-style: italic;">[paʊnd aʊt]</h2>
 
-<div class="content">
+<div class="content" style="padding-bottom: 30px;">
     <div class="row">
-    <p>All the code for my #OUTs can be found at <a href="https://github.com/n0rwin/pound_out" style="font-weight: bold;">my github repository</a></p>
+        <p>All the code for my #OUTs can be found at <a href="https://github.com/n0rwin/pound_out" style="font-weight: bold;">my github repository</a></p>
     </div>
     <div class="row">
         <div class="col-md-4 col-md-offset-2">
@@ -17,6 +17,27 @@ include '../header.php';
             <div class="row">
                 <a href="01_introduction/index.html"><img src="01_introduction/thumb.PNG"></a>
             </div>
+            <p class="row">Published on 2018-02-28</p>
+        </div>
+        <div class="col-md-4">
+            <div class="row">
+                <a href="02_good_artists/index.html" style="font-size: 14pt;">02 - Good Artists ...</a>
+            </div>
+            <div class="row">
+                <a href="02_good_artists/index.html"><img src="02_good_artists/thumb.PNG"></a>
+            </div>
+            <p class="row">Published on 2018-03-11</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-2">
+            <div class="row">
+                <a href="03_no_idea/index.html" style="font-size: 14pt;">03 - No Idea ...</a>
+            </div>
+            <div class="row">
+                <a href="03_no_idea/index.html"><img src="03_no_idea/thumb.PNG"></a>
+            </div>
+            <p class="row">Published on 2018-03-18</p>
         </div>
     </div>
 </div>
